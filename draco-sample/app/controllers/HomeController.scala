77841cf51571e7d97204e4c4337574616068f55e
natsumisawa <natsumi.sawa@bizreach.co.jp>
2019-03-16 13:05:50 +0900

    :sparkles: add tx

--- draco-sample/app/controllers/HomeController.scala
+++ draco-sample/app/controllers/HomeController.scala
@@ -3,11 +3,12 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import java.time.LocalDateTime
 import models.{ Application, Interview }
 
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
-  
+
   def index() = Action { implicit request: Request[AnyContent] =>
 
     import scalikejdbc._
@@ -16,6 +17,9 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
     // Load `application.conf` and setup.
     DBs.setupAll()
 
+    /**
+      * DB: 新しいコネクションを自動的に取得する
+      */
     DB autoCommit {implicit session =>
       sql"""
         create table applications (
@@ -25,6 +29,20 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
         )
       """.execute.apply()
     }
+    // この時点ではコネクションは既に close されている
+
+    /**
+      * localTx: トランザクションブロック
+      */
+    DB localTx { implicit session =>
+      // --- トランザクション開始 ---
+      val (app, int) = (Application.column, Interview.column)
+      withSQL {
+        insert.into(Application).columns(app.name, app.createdAt)
+          .values("Alice", LocalDateTime.now())
+      }.update.apply()
+      // --- トランザクション終了 ---
+    } // 途中で例外が throw されたらロールバック
 
     Ok(views.html.index("hello"))
   }
--- draco-sample/app/models/Sample.scala
+++ draco-sample/app/models/Sample.scala
@@ -1,9 +1,9 @@
 package models
 
 import scalikejdbc._
-import java.time.ZonedDateTime
+import java.time.LocalDateTime
 
-case class Application(id: Long, name: String, createdAt: ZonedDateTime)
+case class Application(id: Long, name: String, createdAt: LocalDateTime)
 
 // scalikejdbc.SQLSyntaxSupport は ResultSet とモデル間の変換まわりの記述量を減らすために定義されている trait
 object Application extends SQLSyntaxSupport[Application] {
@@ -11,11 +11,11 @@ object Application extends SQLSyntaxSupport[Application] {
   override val tableName: String = "applications"
 
   def apply(user: ResultName[Application])(rs: WrappedResultSet): Application = {
-    Application(rs.long(user.id), rs.string(user.name), rs.dateTime(user.createdAt))
+    Application(rs.long(user.id), rs.string(user.name), rs.localDateTime(user.createdAt))
   }
 }
 
-case class Interview(id: Long, applicationId: Long, createdAt: ZonedDateTime)
+case class Interview(id: Long, applicationId: Long, createdAt: LocalDateTime)
 
 object Interview extends SQLSyntaxSupport[Interview] {
 
@@ -23,6 +23,6 @@ object Interview extends SQLSyntaxSupport[Interview] {
   override val tableName: String = "interviews"
 
   def apply(int: ResultName[Interview], app: ResultName[Application])(rs: WrappedResultSet): Interview = {
-    Interview(rs.long(int.id), rs.long(int.applicationId), rs.dateTime(int.createdAt))
+    Interview(rs.long(int.id), rs.long(int.applicationId), rs.localDateTime(int.createdAt))
   }
 }
